5235de6d8f6aa4d62b6b27dee319af41198d4cfb
jmartinez3421 <63542476+jmartinez3421@users.noreply.github.com>
2021-12-07 18:03:54 +0100

    Update BDD.php

--- PHP/BDD.php
+++ PHP/BDD.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "qafe943.misdietas.es";
-    $database = "qafe943";
-    $username = "qafe943";
-    $password = "Patata222";
+    $servername = "**********";
+    $database = "**********";
+    $username = "**********";
+    $password = "**********";
 
     $con = mysqli_connect($servername, $username,$password,$database) or die("Ha habido un error al conectar con la BDD");
 ?>
